aea608b4470b07e398059c01f4fc8c1c349717dd
Nava <qgv3981g@duck.com>
2024-02-27 13:26:04 -0800

    AAA

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
 
+// Allow warnings because they are annoying when debugging
 #[allow(warnings)]
 
+// Define the core modules as existent
+pub mod core;
+
+// Create the new main function
 fn main() {
-    
+
 }
