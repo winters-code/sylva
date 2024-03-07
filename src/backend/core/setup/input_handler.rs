6a733af441d8cf1b228109a5df32d7e907836989
winterscode <qgv3981g@duck.com>
2024-03-07 14:55:40 -0800

    Work on creating inputs

--- src/backend/core/setup/input_handler.rs
+++ src/backend/core/setup/input_handler.rs
@@ -23,14 +23,21 @@ impl InputHandler {
         }
     }
 
-    fn set_key(&self, key: &str, state: InputState) {
-        todo!()
+    fn set_key(&mut self, key: &str, state: InputState) {
+        if let Some(x) = key.get_name() {
+            self._input_states.get_mut()
+        } else {
+            warn!("Key doesn't have name!");
+        }
     }
 
     fn handle_event(&self, e: glfw::WindowEvent) {
         match e {
-            glfw::WindowEvent::Key(Key::Escape, _, Action::Press, _) => {
-                self.set_key("esc", InputState::JustPressed);
+            glfw::WindowEvent::Key(key, _, action, _) => {
+                let input_state = if action == Action::Press { InputState::JustPressed }
+                                else if action == Action::Repeat { InputState::Down }
+                                else { InputState::JustReleased };
+                self.set_key(key.get_name(), input_state);
             },
             _ => {}
         }
